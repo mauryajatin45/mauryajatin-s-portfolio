4a4f90862ae850944b5ce07da89ee6737ce226fd
Jatin Maurya <mauryajatin45@gmail.com>
2024-07-04 04:48:16 +0530

    commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    var nav = document.querySelector('.nav');
+    var hamburger = document.querySelector('.hamburger');
+    nav.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  }
+  
